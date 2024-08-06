9d8898a508895c013146d650829e233f79d44b49
sinjanthu <102690976+Sinjanthu@users.noreply.github.com>
2024-08-06 21:38:15 +0200

    first

---
+++ android/app/src/main/kotlin/com/example/xylophoneny/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.xylophoneny
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+void playSound(int noteName) {
+  AudioCache.instance = AudioCache(prefix: '');
+  final player = AudioPlayer();
+  player.play(AssetSource('assets/note$noteName.wav'));
+}
+
+void buildKeys({int notenr = 1, String colorName1}) {
+  Expanded(
+    child: TextButton(
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.colorName1)),
+      onPressed: () {
+        playSound(notenr);
+      },
+      child: Text(''),
+    ),
+  );
+}
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // buildKeys(1,"pink");
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.purple)),
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue)),
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green)),
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.yellow)),
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.orange)),
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.red)),
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
